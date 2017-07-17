49d27fbda0565c5ad6bb98232e21763d25c1a352
Matt <matthew.hillery@my.jcu.edu.au>
2017-07-17 18:54:42 +1000

    Added notifications

--- fileRemover.py
+++ fileRemover.py
@@ -1,3 +1,4 @@
+from win10toast import ToastNotifier
 import os
 import time
 import sys
@@ -5,10 +6,14 @@ import sys
 path = r"C:\Users\matt-\PycharmProjects\survCamFileRemover\testDirectory"
 
 now = time.time()
-
+file_count = 0
 for f in os.listdir(path):
     f = os.path.join(path, f)
     if os.stat(f).st_mtime < now:    # - 2 * 86400:
+        file_count += 1
         print(str(f))
         if os.path.isfile(f):
             os.remove(os.path.join(path, f))
+
+toaster = ToastNotifier()
+toaster.show_toast("Surveillance", str(file_count) + " files deleted", duration=10)
